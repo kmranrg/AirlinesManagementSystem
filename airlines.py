96c26e416240eb7810036a16daa91da2bc87205c
Kumar Anurag <kan@Kumars-MacBook-Air.local>
2023-04-21 23:43:56 +0530

    basic navigation bar

---
+++ airlines.py
@@ -0,0 +1,22 @@
+import flet as ft
+
+def main(page: ft.Page):
+	page.title = "Airlines Management System"
+	page.theme_mode = "light"
+
+	page.appbar = ft.AppBar(
+		leading=ft.Row([ft.Container(width=4),ft.Image(src="logo/airplane.png",width=50)]),
+		leading_width=60,
+		title=ft.Text("Airlines Management System",color="#FFFFFF"),
+		center_title=False,
+		bgcolor=ft.colors.BLUE_900,
+		actions=[
+			ft.IconButton(ft.icons.WB_SUNNY_OUTLINED,icon_color=ft.colors.WHITE),
+			ft.IconButton(ft.icons.FILTER_3,icon_color=ft.colors.WHITE),
+			ft.Container(width=10)
+		],
+	)
+	
+	page.add(ft.Text('Body'))
+
+ft.app(target=main,assets_dir="assets")
